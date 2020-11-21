8ad33578a1d4b93589d426fb37da791d7a2c7f82
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-22 00:51:29 +0530

    Update config.py

--- assistant/config.py
+++ assistant/config.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+# Copyright (C) 2020 by LovedeepViRk@Github, < https://github.com/Lovedeep-ViRk >.
 #
-# This file is part of < https://github.com/UsergeTeam/Userge-Assistant > project,
+# This file is part of < https://github.com/Lovedeep-ViRk/NinjaDeep-Assistant > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Userge-Assistant/blob/master/LICENSE >
+# Please see < https://github.com/NinjaDeep-Assistant/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -21,17 +21,14 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", 0))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    AUTH_CHATS = set([-1001481357570])  # @UserGeOt
+    AUTH_CHATS = set([-1001194584100  # @NinjaDeepOT
     if os.environ.get("AUTH_CHATS"):
         AUTH_CHATS.update(map(int, os.environ.get("AUTH_CHATS").split()))
-    WHITELIST_CHATS = set([-1001465749479])  # @UserGeSpam
+    WHITELIST_CHATS = set([-1001411940968])  # @NinjaDeepSpam
     if os.environ.get("WHITELIST_CHATS"):
         WHITELIST_CHATS.update(map(int, os.environ.get("WHITELIST_CHATS").split()))
     DEV_USERS = (
-        1158855661,  # @Krishna_Singhal
-        1110621941,  # @PhycoNinja13b
-        921420874,   # @juznem
-        837784353    # @rking_32
+        1470165323,  # @Lovedeep_ViRk
     )
     ADMINS = {}
     MAX_MSG_LENGTH = 4096
