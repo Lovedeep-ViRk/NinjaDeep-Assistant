b486bfd6a9f05a4ee3a429e355ebb84e09e55816
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-22 17:37:16 +0530

    Update config.py

--- assistant/config.py
+++ assistant/config.py
@@ -21,7 +21,7 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", 0))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    AUTH_CHATS = set([-1001194584100  # @NinjaDeepOT
+    AUTH_CHATS = set([-1001194584100])  # @NinjaDeepOT
     if os.environ.get("AUTH_CHATS"):
         AUTH_CHATS.update(map(int, os.environ.get("AUTH_CHATS").split()))
     WHITELIST_CHATS = set([-295066011])  # @NinjaDeepSpam
