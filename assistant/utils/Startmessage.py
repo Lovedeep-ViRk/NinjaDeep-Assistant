4d931970c6f5eaa45a0d585ece7400f63e92d0d9
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-22 19:44:25 +0530

    Delete Startmessage.py

--- assistant/utils/Startmessage.py
+++
@@ -1,6 +0,0 @@
-#  NinjaDeep - Assistant
-# Copyright By @Lovedeep_ViRk
-
-HELP = (	
-    "Welcome To NinjaDeep Assistant This Bot Made For NinjaDeep UserBot Any Help So Visit @NinjaDeepSUPPORT/n ~Thanks"
-  )
