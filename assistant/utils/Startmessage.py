dce4fbd4273d99500be9a9dab90f553954af2fb0
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-22 19:34:50 +0530

    Update Startmessage.py

--- assistant/utils/Startmessage.py
+++ assistant/utils/Startmessage.py
@@ -1,3 +1,6 @@
 #  NinjaDeep - Assistant
 # Copyright By @Lovedeep_ViRk
 
+HELP = (	
+    "Welcome To NinjaDeep Assistant This Bot Made For NinjaDeep UserBot Any Help So Visit @NinjaDeepSUPPORT/n ~Thanks"
+  )
